8f212240ba78777da0a230e7df751e3f0e1b121e
kasra130 <75147013+kasra130@users.noreply.github.com>
2021-04-07 10:59:59 +0300

    Update beeap20.py

--- beeap20.py
+++ beeap20.py
@@ -1,6 +1,6 @@
 # todo: rename the writeYourThought function to yourname_writeYourThought
 # print whatever you want to say to the rest of your peers
 # no nsfw!
-def writeYourThought():
-    print("your Thoughts")
+def Caspar_writeYourThought():
+    print("PID control in simulink is giving me a headache")
     
