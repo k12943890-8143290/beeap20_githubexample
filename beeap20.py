3d9a8265fa7561ab4d16d2ddd5dbc0d1323856a5
Khoa Dang <khoa.dang@hamk.fi>
2021-04-07 11:34:54 +0300

    modified writeYourThought as requested

--- beeap20.py
+++ beeap20.py
@@ -1,6 +1,6 @@
 # todo: rename the writeYourThought function to yourname_writeYourThought
 # print whatever you want to say to the rest of your peers
 # no nsfw!
-def Caspar_writeYourThought():
-    print("PID control in simulink is giving me a headache")
+def khoatuni_writeYourThought():
+    print("git ezpz")
     
